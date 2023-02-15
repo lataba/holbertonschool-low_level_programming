18c9cdf701db7f859363ce035f7254459c674c0e
lataba <ltballeste@gmail.com>
2023-02-15 09:29:56 -0800

    task5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define TASKWORDS "with proper grammar, but the outcome is a piece of art,\n"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(TASKWORDS);
+	return (0);
+}
