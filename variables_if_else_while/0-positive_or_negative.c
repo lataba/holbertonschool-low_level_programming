b7140da32fbab99ddb6827a07b9cf8c410e3ea05
lataba <ltballeste@gmail.com>
2023-02-16 09:04:32 -0800

    task0 fixing betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'Number positive or negative'
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	int n;
