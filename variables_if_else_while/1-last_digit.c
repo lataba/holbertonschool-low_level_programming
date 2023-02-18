ee3e178cfa820ab0ef8f52baa83308816f51b16b
lataba <ltballeste@gmail.com>
2023-02-18 09:32:01 -0800

    Task1, condition while !=wq

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,14 +17,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ultimo = n;
-	while (ultimo > 9)
+	while ((ultimo > 9) != (ultimo < -9))
 		ultimo = ultimo % 10;
 	printf("Last digit of %d is ", n);
 	if (ultimo == 0)
 		printf("%d and is 0", ultimo);
 	else if (ultimo > 5)
-		printf("%d and id greater than 5", ultimo);
+		printf("%d and is greater than 5", ultimo);
 	else
-		printf("%d and is less than 6 and not 0",ultimo);
+		printf("%d and is less than 6 and not 0", ultimo);
 	return (0);
 }
