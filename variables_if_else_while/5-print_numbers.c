8c0562b113b51922cbe2f46782b4e758b3aa2ca3
lataba <ltballeste@gmail.com>
2023-02-18 15:06:38 -0800

    Tasks 4 & 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Numbers 0 to 9'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
