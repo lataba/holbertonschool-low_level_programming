dfc4ff45166d933efaf10dbf010982097c925e4b
lataba <ltballeste@gmail.com>
2023-02-18 19:59:10 -0800

    task8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Ultimo digito'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num ;
+
+	char letter;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
