bd01a916ae81564ae8e2905c897b3e6dbe681809
lataba <ltballeste@gmail.com>
2023-02-19 10:31:57 -0800

    task11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 - Prints first 98 natural numbers.
+ *
+ * @n: number.
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
