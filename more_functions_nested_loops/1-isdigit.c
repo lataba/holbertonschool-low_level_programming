fd182f7ff6f39e82881d4e24ebb7b4dcfc613534
lataba <ltballeste@gmail.com>
2023-02-23 09:46:54 -0800

    task1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - cheks for uppercase character
+ *
+ * @c: symbol to be checked
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
+
