de5a69b8c08cc70fe10b5add90e07ce0a17163ad
lataba <ltballeste@gmail.com>
2023-02-24 13:51:04 -0800

    task7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_diagonal - Prints a diagonal.
+*
+*@n : times \ repeat
+*
+* Return: diagonal of \
+*/
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+}
