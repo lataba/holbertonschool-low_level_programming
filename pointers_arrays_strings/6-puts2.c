a426aee8c916a5c4000ea9a66c894a1633696fc2
lataba <ltballeste@gmail.com>
2023-02-27 15:06:55 -0800

    task6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str : string
+ *
+ * Return: @str reverse
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
