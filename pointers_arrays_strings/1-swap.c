e134c24555417859f7f2a73af310f1b7035588a4
lataba <ltballeste@gmail.com>
2023-02-27 07:12:50 -0800

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ *
+ * @a: int
+ *
+ * @b: int
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
