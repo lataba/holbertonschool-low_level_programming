79c9bc97b92e0fbde095d8080fbee3d27433d9c5
lataba <ltballeste@gmail.com>
2023-02-28 07:15:19 -0800

    task 100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts string on integer
+ *
+ * @s : string
+ *
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	int i, sign;
+	
+	unsigned int n;
+
+	i = n = 0;
+
+	sign = 1;
+
+	while ((s[i] < '0') || ((s[i] > '9') && (s[i] != '\0')))
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while ((s[i] >= '0') && (s[i] <= '9') && (s[i] != '\0'))
+	{
+			n = (n * 10) + (s[i] - '0');
+			i++;
+	}
+	return (n*sign);
+}
