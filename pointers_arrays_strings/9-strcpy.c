0b199f7fdc6090437acf73996f7c31eb94e70c4e
lataba <ltballeste@gmail.com>
2023-02-27 17:16:59 -0800

    task9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy the string
+ *
+ * @src : string to copy
+ *
+ * @dest : pointer to a copy
+ *
+ * Return: a pointer to a string copy
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
