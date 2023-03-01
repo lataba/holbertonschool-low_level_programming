4190f7c253f29c4033d47987c7b5009e429790c2
lataba <ltballeste@gmail.com>
2023-03-01 09:00:38 -0800

    task4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - array to reverse
+ *
+ * @a : array
+ *
+ * @n : elements quantity
+ *
+ * Return: @a reverse
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, aux;
+
+	i = 0;
+
+	for (i = 0 ; i <=  ((n - 1) / 2) ; i++)
+	{
+		aux = a[i];
+		a[i] = a[(n - 1) - i];
+		a[(n - 1) - i] = aux;
+	}
+}
