471d425405fc588c1e6bc3bd88eac17fc0570098
lataba <ltballeste@gmail.com>
2023-03-03 08:59:34 -0800

    task6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,15 +11,19 @@
 char *cap_string(char *str)
 
 {
-	int i = 0, isep = 0;
+	int i = 0, isep = 0, separetor;
 
 	char sep[] = {32, '\t', '\n', 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 
-	while (str[i] != '\0')
+	while (str[i])
 	{
+		for (isep = 0; isep <= 12; isep++)
+			if ((i != 0) && (str[i - 1] == sep[isep]))
+				separetor = isep;
+
 		if (((str[i] >= 97) && (str[i] <= 122)) &&
-				((i == 0) || (str[i - 1] == sep[isep])))
+				((i == 0) || (str[i - 1] == sep[separetor])))
 			str[i] = str[i] - 32;
 		i++;
 	}
