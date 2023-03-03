c885d794937d2e229f40178f86fa5f8ca83691db
lataba <ltballeste@gmail.com>
2023-03-03 06:49:36 -0800

    task5 fixing pointer

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,11 +11,13 @@
 char *string_toupper(char *str)
 
 {
-	while (*str)
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-		if ((*str >= 97) && (*str <= 122))
-			*str = *str - 32;
-		str++;
+		if ((str[i] >= 97) && (str[i] <= 122))
+			str[i] = str[i] - 32;
+		i++;
 	}
 	return (str);
 }
