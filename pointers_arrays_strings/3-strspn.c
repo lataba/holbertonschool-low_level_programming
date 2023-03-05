108e9bb5d1a6c6772404a452163b9c247480f80a
lataba <ltballeste@gmail.com>
2023-03-05 13:58:11 -0800

    task3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -14,15 +14,21 @@ unsigned int _strspn(char *s, char *accept)
 
 {
 	int i = 0;
-	unsigned int n = -1;
+	unsigned int n = 0;
 
-	while ((*s++) && (*s != '\0'))
+	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
-			if ((*s == accept[i]) && (*s != '\0'))
+			if (*s == accept[i])
+			{
 				n++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (n);
 		}
+		s++;
 	}
 	return (n);
 }
