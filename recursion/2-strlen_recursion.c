657ad02918ac89fb9dab0998e2c9a3df70454588
lataba <ltballeste@gmail.com>
2023-03-06 10:57:53 -0800

    task2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string
+ */
+
+int _strlen_recursion(char *s)
+
+{
+	int i = 0;
+	if (*s)
+	{
+		i++;
+		i = i + _strlen_recursion(s + 1);
+	}
+	return (i);
+}
