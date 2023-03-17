5d1e5f1ef81edb5de3477530155ba11fa0e137ca
lataba <ltballeste@gmail.com>
2023-03-17 08:57:27 -0700

    task2 fixing NULL NULL entry

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -40,7 +40,11 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j, stop1, stop2;
 
 	if ((s1 == NULL) && (s2 == NULL))
-		return (NULL);
+	{
+		concstr = malloc(sizeof(char));
+		concstr[1] = '\0';
+		return (concstr);
+	}
 	if (s1 == NULL)
 		return (_strdup(s2));
 	if (s2 == NULL)
