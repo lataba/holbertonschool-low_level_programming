c9bfb475aa4d4aebebe24683bd36b4ea0fe6fcd1
lataba <ltballeste@gmail.com>
2023-03-17 08:44:51 -0700

    task2 fixing NULL entry

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * _strdup - function that copy a string
+ * @str: entry array
+ * Return: A pointer to de array
+ */
+
+char *_strdup(char *str)
+{
+	char *cpstr = NULL;
+	int i, strtop;
+
+	if (str == NULL)
+		return (NULL);
+
+	strtop = strlen(str) + 1;
+
+	cpstr = malloc(strtop * sizeof(char));
+	if (cpstr == NULL)
+		return (NULL);
+	for (i = 0; i < strtop; i++)
+		cpstr[i] = str[i];
+	return (cpstr);
+}
 
 /**
  * str_concat - function that concatenate two strings
