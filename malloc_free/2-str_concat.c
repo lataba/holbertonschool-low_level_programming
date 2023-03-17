603739a28bbd5398e3f35431882df1c34acd55db
lataba <ltballeste@gmail.com>
2023-03-17 08:15:49 -0700

    task2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+/**
+ * str_concat - function that concatenate two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: a pointer to concatenate strings
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concstr = NULL;
+	int i = 0, j, stop1, stop2;
+
+	if (s1 == NULL)
+		return (s2);
+	if (s2 == NULL)
+		return (s1);
+
+	stop1 = strlen(s1);
+	stop2 = strlen(s2) + 1;
+
+	concstr = malloc((stop1 + stop2) * sizeof(char));
+	if (concstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < stop1; i++)
+		concstr[i] = s1[i];
+	for (j = 0; j < stop2; j++)
+	{
+		concstr[i] = s2[j];
+		i++;
+	}
+	return (concstr);
+}
