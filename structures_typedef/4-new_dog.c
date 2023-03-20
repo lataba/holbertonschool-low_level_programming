d19e8b36fe1436adc4565ff13a24b944ccb93aa0
lataba <ltballeste@gmail.com>
2023-03-20 14:42:26 -0700

    task4 fixing malloc and free

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,8 @@
 #include "dog.h"
+#include <string.h>
 
 /**
- * new_dog -  function that initialize a variable of type struct dog
+ * new_dog - function that initialize a variable of type struct dog
  * @name: dog's name
  * @age: dog's age
  * @owner: dog owner name
@@ -13,17 +14,30 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog = NULL;
 
-	if ((name == NULL) || (owner == NULL))
-		return (NULL);
-
 	newdog = malloc(sizeof(dog_t));
-
 	if (newdog == NULL)
+	{
+		free(newdog);
 		return (NULL);
+	}
 
-	newdog->name = name;
+	newdog->name = malloc(strlen(name) + 1);
+	if (newdog->name == NULL)
+	{
+		free(newdog);
+		return (NULL);
+	}
+
+	newdog->owner = malloc(strlen(owner) + 1);
+	if (newdog->owner == NULL)
+	{
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+	}
+	strcpy(newdog->name, name);
 	newdog->age = age;
-	newdog->owner = owner;
+	strcpy(newdog->owner, owner);
 
 	return (newdog);
 }
