cea681511d4d2b117fdc82bcd7ca536e473d24fa
lataba <ltballeste@gmail.com>
2023-03-20 09:17:37 -0700

    task2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints a struct dog
+ * @d: the info to be printed
+ */
+
+void print_dog(struct dog *d)
+
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", (*d).name);
+
+	printf("Age: %f\n", (*d).age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", (*d).owner);
+}
