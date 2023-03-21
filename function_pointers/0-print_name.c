82a64b041aed0f442588030454c1526fb56f102c
lataba <ltballeste@gmail.com>
2023-03-21 13:01:48 -0700

    task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+include "function_pointers.h"
+#include <string.h>
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: a function pointer that takes char and returns void
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+
+{
+		f(name);
+}
