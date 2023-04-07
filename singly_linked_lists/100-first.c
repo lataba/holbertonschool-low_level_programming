74dde819114d1c646dcf4b8c5fee3c91b36a910c
lataba <ltballeste@gmail.com>
2023-04-07 14:29:29 -0700

    Extra task tourtle + lists.h

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * tourtle - function that prints a sentense
+ *
+ * Return: Always 0
+ */
+
+void tourtle(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
