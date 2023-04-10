ff097e654e22eef7946e759d1c5220d2d8bca3d4
lataba <ltballeste@gmail.com>
2023-04-10 16:15:06 -0700

    task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -7,38 +7,11 @@
  * @n: data for the new node
  * Return: The new node
  */
-size_t dlistint_len(const dlistint_t *h);
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0, len = 0;
 	dlistint_t *new = NULL, *curr = NULL;
-/*
-	curr = *h;
-	if (idx == i)
-		return (add_dnodeint(h, n));
-	else
-	{
-		while (curr->next != NULL && i < (idx - 1))
-		{
-			curr = curr->next;
-			i++;
-		}
-		if (idx == i)
-			return (add_dnodeint_end(h, n));
-		if (i == (idx - 1))
-		{
-			new = (malloc(sizeof(dlistint_t)));
-			if (new == NULL)
-				return (NULL);
-			new->n = n;
-			new->next = curr->next;
-			curr->next = new;
-			new->prev = curr;
-		}
-	}
-	return (new);
-}*/
 
 	len = dlistint_len(*h);
 	curr = *h;
@@ -49,31 +22,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	if (len == idx + 1)
 		return (add_dnodeint_end(h, n));
-	else
+	while (curr->next != NULL && i < (idx - 1))
 	{
-		while (curr->next != NULL && i < (idx - 1))
-		{
-			curr = curr->next;
-			i++;
-		}
-		if (i == (idx - 1))
-		{
-			new = (malloc(sizeof(dlistint_t)));
-			if (new == NULL)
-				return (NULL);
-			new->n = n;
-			new->next = curr->next;
-			curr->next = new;
-			new->prev = curr;
-		}
+		curr = curr->next;
+		i++;
+	}
+	if (i == (idx - 1))
+	{
+		new = (malloc(sizeof(dlistint_t)));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->next = curr->next;
+		curr->next = new;
+		new->prev = curr;
 	}
 	return (new);
 }
 
 /**
- * dlistint_len - Returns the length of a linked list
- * @h: Head of the linked list
- * Return: The length of the linked list
+ * dlistint_len - Returns the length of a list
+ * @h: Head of the list
+ * Return: The length of the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
