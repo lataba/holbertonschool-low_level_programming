b1b0a485cd12c8fad19f06150b5eed84dbd35d36
lataba <ltballeste@gmail.com>
2023-04-10 17:54:59 -0700

    task7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,60 +10,38 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0, len = 0;
-	dlistint_t *new = NULL, *curr = NULL;
+	dlistint_t *new = NULL, *curr;
+	unsigned int i = 0;
 
-	len = dlistint_len(*h);
 	curr = *h;
+
+	if (h == NULL || (curr == NULL && idx != 0))
+		return (NULL);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-
-	if (len < idx + 1)
+	while (curr != NULL)
 	{
-		free(new);
-		return (NULL);
-	}
-	if (idx == 0)
-	{
-		new->prev = NULL;
-		new->next = curr;
-		if (curr != NULL)
-			(curr)->prev = new;
-		curr = new;
-		return (new);
-	}
-	if (len == idx + 1)
-		return (add_dnodeint_end(h, n));
-	while (curr->next != NULL && i < (idx - 1))
-	{
+		if (idx == i)
+		{
+			new->prev = curr->prev;
+			new->next = curr;
+			(curr->prev)->next = new;
+			curr->prev = new;
+			return (new);
+		}
+		if ((idx - 1) == i && curr->next == NULL)
+		{
+			return (add_dnodeint_end(h, n));
+		}
 		curr = curr->next;
 		i++;
 	}
-	if (i == (idx - 1))
-	{
-		new->next = curr->next;
-		curr->next = new;
-		new->prev = curr;
-	}
-	return (new);
-}
-
-/**
- * dlistint_len - Returns the length of a list
- * @h: Head of the list
- * Return: The length of the list
- */
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t len = 0;
-
-	while (h)
-	{
-		len++;
-		h = h->next;
-	}
-	return (len);
+	free(new);
+	return (NULL);
 }
