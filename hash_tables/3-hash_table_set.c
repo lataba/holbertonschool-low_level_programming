9dc176b822943ed84ac2c286858c942b847b2c7e
lataba <ltballeste@gmail.com>
2023-04-16 18:47:52 -0700

    task3 fixing compile

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <string.h>
+#include <stdlib.h>
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
